08a5e45f07e7fcbfc74a4882708554e6d88ce131
Ryan Stack <rms332@yahoo.com>
2014-04-22 08:18:15 -0600

    pseudocode and initial solution

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -9,11 +9,36 @@
 # 1. Pseudocode
 
 # What is the input?
+
+#For Array method it is an array of numbers.  For sentence_maker, it is an array of strings and numbers.
 # What is the output? (i.e. What should the code return?)
+#For Array, the output is the sum of all the numbers in the array.  For sentence_maker 
+#the output is a sentence where the first word is capitalized and ends with a period.
 # What are the steps needed to solve the problem?
+#Define Array method.
+#Create variable total, assigned to value 0.
+#Loop through array, and each element should be added to total.
+#Return Total.
+ 
+ #Define sentence_maker method.  
+ #create string variable sentence.
+ #Assign variable to a a join method called on array.
+ #Modify sentence to capitablize first word and add a period to the end.
+ #Return sentence.   
 
 
 # 2. Initial Solution
+def total(Array) 
+	Array.sum
+end
+
+
+def sentence_maker(Array)
+	sentence = Array.join(',')
+	sentence.capitalized!
+	sentence << "."
+end
+
 
 
 
@@ -23,3 +48,8 @@
 
 
 # 4. Reflection 
+
+
+
+
+
