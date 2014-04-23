3a5cf73ca765d881a845520fd0fb99811fc11237
Ryan Stack <rms332@yahoo.com>
2014-04-22 20:13:25 -0600

    initial

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -8,11 +8,46 @@
 # 1. Pseudocode
 
 # What is the input?
+
+#An integer 
+
 # What is the output? (i.e. What should the code return?)
+
+#A comma-separated integer as a string
+
 # What are the steps needed to solve the problem?
+
+#assign the integer parameter to a string
+#Starting from the right hand side, iterate through the string, pushing the value to the front of a new array.  After every third byte,
+#if there is a third byte, inert a comma.
+#Join araay 
+
 
 
 # 2. Initial Solution
+def number_fixer(num)
+	str = num.to_s
+	str.reverse!
+	commas = []
+	count = 0
+	str.split("").each do |i|
+		if count < 3
+			commas.unshift(i)
+			count += 1
+		else 
+			commas.unshift(",")
+			count = 0
+		end 
+	end
+	commas.join("")
+end 
+
+print number_fixer(100000)
+
+
+		
+
+
 
 
 
