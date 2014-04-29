59e80ee5e21462d656d60d16ee44a28f6e84961b
Ryan Stack <rms332@yahoo.com>
2014-04-29 09:12:28 -0600

    all but driver

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,28 +1,37 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input:  The number of sides that the die will have.
+# Output:  A class that can create die with a certain amount of sides and each die
+		# object will have a funtion that identifies how many dies they have, and then you will be 
+		#able to "roll" the die, and the random number within the range will be printed.
 # Steps:
+#Define the class die.
+#Initialize the die, including the ArgumentError conditional branch
+#Define the side method
+#Define the roll method.
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+    unless sides > 0 
+    	raise ArgumentError.new("sides must be greater than or equal to 1.")
+    end
+    @sides = sides
   end
   
   def sides
-    # code goes here
+    @sides
   end
   
   def roll
-    # code goes here
+    1 + rand(@sides)
   end
 end
 
@@ -37,8 +46,7 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
-
+#puts Die.new(0) == "ArgumentError: sides must be greater than or equal to 1."
 
 
 
